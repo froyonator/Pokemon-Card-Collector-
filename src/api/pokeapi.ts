20a101a0f69a680400dee3328f1c4506a8e049b8
froyonator <276264696+froyonator@users.noreply.github.com>
2026-07-10 14:53:46 +1000

    Add PokeAPI sprite URL helper

---
+++ src/api/pokeapi.ts
@@ -0,0 +1,3 @@
+export function spriteUrl(dexNumber: number): string {
+  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${dexNumber}.png`;
+}
